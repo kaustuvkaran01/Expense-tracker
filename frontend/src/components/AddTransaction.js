a9817cf30aa017f5c632d330a2a1af91a61e46df
Kaustuv Karan <kaustuvkaran01@gmail.com>
2021-08-08 17:19:38 +0530

    chore:add transactions refactored

---
+++ frontend/src/components/AddTransaction.js
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export default function AddTransaction() {
+	const [text, setText] = useState("");
+	const [amount, setAmount] = useState(0);
+	const { addTransaction } = useContext(GlobalContext);
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const newTransaction = {
+			id: Math.floor(Math.random() * 100000),
+			text,
+			amount: +amount,
+		};
+		console.log(newTransaction);
+		addTransaction(newTransaction);
+	};
+	return (
+		<div>
+			<h3>Add new transaction</h3>
+			<form onSubmit={onSubmit}>
+				<div className="form-control">
+					<label htmlFor="text">Text</label>
+					<input
+						type="text"
+						id="text"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+						placeholder="Enter text..."
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="amount">
+						Amount <br />
+						(negative - expense, positive - income)
+					</label>
+					<input
+						type="number"
+						id="amount"
+						value={amount}
+						onChange={(e) => setAmount(e.target.value)}
+						placeholder="Enter amount..."
+					/>
+				</div>
+				<button type="submit" className="btn">
+					Add a new transaction
+				</button>
+			</form>
+		</div>
+	);
+}
