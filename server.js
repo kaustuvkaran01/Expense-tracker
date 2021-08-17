7e182625cf686f0a80762c10c33bf8feb46e84ef
Kaustuv Karan <kaustuvkaran01@gmail.com>
2021-08-17 14:36:46 +0530

    chore: Frontend integrated with backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -24,6 +25,14 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/transactions", transactions);
 
 // app.get("/", (req, res) => res.send("Hello"));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
 
 app.listen(
 	PORT,
